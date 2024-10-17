f0525521130e276394f956dcac994746ce0c84e7
Juan Araujo <juan110588@gmail.com>
2024-10-17 11:29:54 -0300

    Tirando de vez login de cooperativa

--- src/main/java/recoope/api/services/CustomUserDetailService.java
+++ src/main/java/recoope/api/services/CustomUserDetailService.java
@@ -6,41 +6,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import recoope.api.domain.entities.Cooperativa;
 import recoope.api.domain.entities.Empresa;
-import recoope.api.repository.ICooperativaRepository;
 import recoope.api.repository.IEmpresaRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailService implements UserDetailsService {
-    private final ICooperativaRepository _cooperativaRepository;
     private final IEmpresaRepository _empresaRepository;
 
-    public CustomUserDetailService(ICooperativaRepository cooperativaRepository, IEmpresaRepository empresaRepository) {
-        _cooperativaRepository = cooperativaRepository;
+    public CustomUserDetailService(IEmpresaRepository empresaRepository) {
         _empresaRepository = empresaRepository;
     }
 
     public UserDetails loadUserByUsername(String cnpjOuEmail) throws UsernameNotFoundException{
         Optional<Empresa> empresaOpt = _empresaRepository.login(cnpjOuEmail);
 
-        if (!empresaOpt.isPresent()) {
-            Cooperativa cooperativa = _cooperativaRepository.login(cnpjOuEmail)
-                    .orElseThrow(() -> new UsernameNotFoundException("Usuário ou senha inválidos"));
-
-            return new User(cooperativa.getCnpj(),
-                    cooperativa.getSenha(),
-                    true,
-                    true,
-                    true,
-                    true,
-                    List.of(new SimpleGrantedAuthority("ROLE_COOPERATIVA"))
-            );
-        } else {
+        if (empresaOpt.isPresent()) {
             Empresa empresa = empresaOpt.get();
             return new User(empresa.getCnpj(),
                     empresa.getSenha(),
@@ -51,5 +34,7 @@ public class CustomUserDetailService implements UserDetailsService {
                     List.of(new SimpleGrantedAuthority("ROLE_EMPRESA"))
             );
         }
+
+        throw new UsernameNotFoundException("Usuario não encontrado");
     }
 }
